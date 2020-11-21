977457475b03939053569c005e9464e2d59db5f7
Irvin Shen <irvinshen.texas@gmail.com>
2020-11-20 22:50:40 -0600

    Made the Engineering Stress calculations functional for all graphs Updated ReadMe with a bit more info Created Example Data folder with the files that I'm working with Allowed the final html file to be saved to a directory of the end-user's choosing.

--- Plotting.py
+++ Plotting.py
@@ -14,7 +14,6 @@ print(filename)
 
 
 class Specimen:
-    area = None
 
     def __init__(self, filepath):
         # Create Dataframe for the excel file for all of the tension testing data
@@ -22,27 +21,29 @@ class Specimen:
         self.constants = pandas.read_excel(filepath, header=None, nrows=8, usecols="A:B").transpose()
         self.constants.columns = self.constants.iloc[0]
         self.constants = self.constants.iloc[1]
-        self.diameter = self.constants["Initial Diameter (calipers)"]
+        self.diameter = self.constants.at["Initial Diameter (calipers)"]
         self.area = self.diameter ** 2 / 4 * math.pi
-        self.name = self.constants["Specimen Name"]
-        self.gaugeLength = self.constants["Gauge Length (extensometer)"]
+        self.name = self.constants.at["Specimen Name"]
+        self.gaugeLength = self.constants.at["Gauge Length (extensometer)"]
         self.eng_stress = self.data["Load N"].apply(lambda x, a: x / a, args=(self.area,))
-        self.eng_stress = self.eng_stress.rename({"Load N": "Eng. Stress"})
+        self.eng_stress = self.eng_stress.rename("Eng. Stress")
         self.eng_strain = self.data["Extension(extensometer) mm"].apply(lambda x, e: x / e, args=(self.gaugeLength,))
-        self.eng_strain = self.eng_strain.rename({"Extension(extensometer) mm": "Eng. Strain"})
-        self.eng_data = pandas.DataFrame(self.eng_strain, self.eng_stress)
+        self.eng_strain = self.eng_strain.rename("Eng. Strain")
+        self.eng_data = pandas.concat((self.eng_strain, self.eng_stress), axis=1)
+        print(self.data["Extension(extensometer) mm"])
         print(self.eng_data)
 
     def gen_graphs(self):
-        output_file("Stress_Strain_{}.html".format(self.name))
+        save_location = filedialog.askdirectory()
+        output_file(save_location + r"\Stress_Strain_{}.html".format(self.name))
         source = ColumnDataSource(self.eng_data)
         xlabel = 'Engineering Strain'
         ylabel = 'Engineering Stress'
-        TOOLS = 'pan, wheel_zoom, box_select, reset, save'
-        plot = figure(title="Ln(stress) vs. Ln(strain)", x_axis_label=xlabel, y_axis_label=ylabel,
-                      tools=TOOLS, toolbar_location="below")
-        plot.circle(x="Eng. Strain", y="Eng. Stress", source=source, fill_color="white", radius=.05)
-        show(plot)
+        tools = 'pan, wheel_zoom, box_select, reset, save'
+        eng_plot = figure(title='Engineering \u03C3 vs. \u03B5', x_axis_label=xlabel, y_axis_label=ylabel,
+                          tools=tools, toolbar_location="below")
+        eng_plot.circle(x="Eng. Strain", y="Eng. Stress", source=source)
+        show(eng_plot)
 
     # Create Engineering Stress and Strain data sets
     # eng_stress = [force / area for force in load]
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # ME134LData
-Data visuaizations for a Materials Engineering course
+Data visuaizations for a Materials Engineering course with lab data by using pandas, bokeh, and tkinter. Currently, the script takes a file with 8 properties set above the true data set and puts it into it's own dataframe.
 
+You can choose where you would like the Bokeh HTML file to be saved as well as choose the file using Tkinter.
 
-Still very much a work in progress. Current files are not fully functional.
+All data is visualized as circle points so that the specific points can be selected out. Additionally, you can zoom in and out and move around the dataset.
+
